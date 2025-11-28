d2d50933b9ebb5d66cc97a30daf6b9a686351246
Lucas Teixeira <82039819+lucastdcs@users.noreply.github.com>
2025-11-28 20:21:29 +0000

    vv

--- src/modules/notes/notes-assistant.js
+++ src/modules/notes/notes-assistant.js
@@ -19,7 +19,7 @@ import * as NoteStyles from './notes-styles.js';
 import { copyHtmlToClipboard, ensureNoteCardIsOpen, triggerInputEvents } from './notes-bridge.js';
 
 export function initCaseNotesAssistant() {
-    const CURRENT_VERSION = "v3.4.5"; 
+    const CURRENT_VERSION = "v3.4.7"; 
     
     let currentCaseType = 'bau';
     let currentLang = 'pt'; 
