ce22fbda1e0d344a1539c8efe29788fe4c7aed0a
lucastdcs <lucastdcs@users.noreply.github.com>
2026-01-22 14:39:51 +0000

    deploy: c2bf7184afa5633bd9bc1d4a1bd44883305e9b5a

--- bundle-dev.js
+++ bundle-dev.js
@@ -471,7 +471,7 @@ Irei abrir caso em BAU para o dia solicitado e pedir descarte do mesmo, levando 
 
     /* --- PILL PRINCIPAL --- */
     .cw-pill {
-        position: fixed; top: 30%; right: 24px;
+        position: fixed; top: 90%; right: 24px;
         display: flex; flex-direction: column; align-items: center; gap: 12px;
         padding: 16px 8px;
         
